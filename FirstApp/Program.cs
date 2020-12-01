e2dfca9606463548d851abdc79b297e0c0a1dd1d
TeslaAnna <tesla.anna@gmail.com>
2020-12-01 23:01:11 +0200

    Использование литералов

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -10,6 +10,10 @@ namespace FirstApp
 
             Console.WriteLine(myName);
 
+            Console.WriteLine("Привет, мир!");
+            Console.WriteLine("Мне 34 года");
+            Console.WriteLine("My name is Anna");
+
             Console.ReadKey();
         }
     }
