432f54c56f991a61477dc2c4e3b5654fcad744d0
TeslaAnna <tesla.anna@gmail.com>
2020-12-02 20:10:29 +0200

    Добавление перечислений Semaphore

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -12,6 +12,13 @@ namespace FirstApp
         Saterday,
         Sunday
     }
+
+    enum Semaphore
+    {
+        Red = 100,
+        Yellow = 200,
+        Green = 300
+    }
     class Program
     {
         static void Main(string[] args)
