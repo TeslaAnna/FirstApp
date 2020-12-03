1d6b35eec8815001a018b1da98cb64be644899ea
TeslaAnna <tesla.anna@gmail.com>
2020-12-03 23:22:14 +0200

    Финальная практика модуля «Анкетируем пользователей»

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,46 +1,26 @@
 ﻿using System;
 
 namespace FirstApp
-{
-    enum DaysOfWeek : byte
-    {
-        Monday = 1,
-        Tyesday,
-        Wednesday,
-        Thursday,
-        Friday,
-        Saterday,
-        Sunday
-    }
-
-    enum Semaphore
-    {
-        Red = 100,
-        Yellow = 200,
-        Green = 300
-    }
+{    
     class Program
     {
         static void Main(string[] args)
         {
-            string MyName = "Anna";
-            byte MyAge = 34;
-            bool HaveIApet = false;
-            double MyShoeSize = 35;
+            Console.Write("Enter your name: ");
+            string Name = Console.ReadLine();
 
-            Console.WriteLine("My name is " + MyName);
-            Console.WriteLine("My age is " + MyAge);
-            Console.WriteLine("Do I have a pet? " + HaveIApet);
-            Console.WriteLine("My shoe size is " + MyShoeSize);
+            Console.Write("Enter your age: ");
+            var Age = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("LongMin {0}", long.MinValue);
-            Console.WriteLine("LongMax {0}", long.MaxValue);
+            Console.WriteLine("Your name is {0} and age is {1}", Name, Age);
 
-            Console.WriteLine();
-            Console.WriteLine("\t Привет, \n мир!");
-            Console.WriteLine($"Мой возраст {MyAge}");
+            Console.WriteLine("Enter your birthdate: ");
+            var Birthdate = Console.ReadLine();
 
-            Console.ReadKey();
+            Console.WriteLine("Your birthday is {0}", Birthdate);
+
+
+
         }
     }
 }
