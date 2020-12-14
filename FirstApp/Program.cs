d1d8d942f2be342e487f94198028d2bd405438f1
TeslaAnna <tesla.anna@gmail.com>
2020-12-14 20:31:31 +0200

    4.2.9 и 4.2.10 счетчик для for и для while

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -5,12 +5,14 @@ namespace FirstApp
     class Program
     {
         static void Main(string[] args)
+
         {
-            for (int i = 5; i > 1; i--)
+            Console.WriteLine("Цикл for");
+
+            for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine("Iteration {0}", i);
-
-
+                Console.WriteLine(i);
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
                 switch (Console.ReadLine())
                 {
                     case "red":
@@ -43,11 +45,53 @@ namespace FirstApp
                 }
             }
 
+                Console.WriteLine("Цикл while");
+                int k = 0;
+                
+                while(k<3)
+                {
+                    Console.WriteLine(k);
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
 
-            Console.ReadKey();
+                        Console.WriteLine("Ваш любимы цвет красный!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Ваш любимый цвет зелёный!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Ваш любимый цвет бирюзовый");
+                        break;
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Ваш любимый цвет жёлтый!");
+                        break;
+                }
+                k = k++;
+
+                }
+            }
+
+
+            
 
 
 
         }
     }
-}
+
