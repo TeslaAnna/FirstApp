8e640a9477ed94366429ec0cd375385612821990
TeslaAnna <tesla.anna@gmail.com>
2020-12-14 20:40:33 +0200

    4.2.11 Добавление цикла do...while

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 
 namespace FirstApp
-{    
+{
     class Program
     {
         static void Main(string[] args)
 
         {
+            Console.WriteLine("Цикл do...while");
+            int t = 0;
+
+            do
+            {
+                Console.WriteLine(t);
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Ваш любимы цвет красный!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Ваш любимый цвет зелёный!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Ваш любимый цвет бирюзовый");
+                        break;
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Ваш любимый цвет жёлтый!");
+                        break;
+                }
+                t++;
+            } while(t < 3);
+
+
+
             Console.WriteLine("Цикл for");
 
             for (int i = 0; i < 3; i++)
@@ -45,12 +87,12 @@ namespace FirstApp
                 }
             }
 
-                Console.WriteLine("Цикл while");
-                int k = 0;
-                
-                while(k<3)
-                {
-                    Console.WriteLine(k);
+            Console.WriteLine("Цикл while");
+            int k = 0;
+
+            while (k < 3)
+            {
+                Console.WriteLine(k);
                 Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
                 switch (Console.ReadLine())
                 {
@@ -84,14 +126,14 @@ namespace FirstApp
                 }
                 k = k++;
 
-                }
             }
-
-
-            
-
-
-
         }
+
+
+
+
+
+
     }
+}
 
