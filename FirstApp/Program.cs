1cbc5bd3f138097bd8dc27a479fb618bad632ebe
TeslaAnna <tesla.anna@gmail.com>
2020-12-15 11:44:17 +0200

    4.3.10 Вывод числового массива по столбцам

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -10,11 +10,11 @@ namespace FirstApp
 
             int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
 
-            for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
+            for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
             {
-                for (int k = 0; k < array.GetUpperBound(1) + 1; k++)
+                for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
 
-                    Console.Write(array[i, k] + " ");
+                    Console.Write(array[k, i] + " ");
 
                 Console.WriteLine();
             }
