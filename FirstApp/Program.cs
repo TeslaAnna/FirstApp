aa36bedb704b0b7002de7491a6aed2d39de3c4f3
TeslaAnna <tesla.anna@gmail.com>
2020-12-20 17:28:27 +0200

    5.1 Задание метода ShowColor строкового

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,52 +4,48 @@ namespace FirstApp
 {
     class Program
     {
+        static string ShowColor()
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            var color = Console.ReadLine();
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+
+            }
+            return color;
+        }
         static void Main(string[] args)
 
         {
-            (string Name, string LastName, string Login, int LoginLength, bool HavePet, double Age, string[] Color) User;
 
-            for (int x = 0; x< 3; x++)
-            {
-                Console.WriteLine("Введите имя");
-                User.Name = Console.ReadLine();
-
-                Console.WriteLine("Введите фамилию");
-                User.LastName = Console.ReadLine();
-
-                Console.WriteLine("Введите логин");
-                User.Login = Console.ReadLine();
-                User.LoginLength = User.Login.Length;
-
-                Console.WriteLine("Есть ли у вас животные? Да или Нет");
-                string i = Console.ReadLine();
-
-                if (i == "Да")
-                {
-                    User.HavePet = true;
-                }
-                else
-                {
-                    User.HavePet = false;
-
-                }
-
-                Console.WriteLine("Введите возраст пользователя");
-                User.Age = Convert.ToDouble(Console.ReadLine());
-
-                User.Color = new string[3];
-
-                Console.WriteLine("Введите три любимых цвета пользователя");
-
-                for (int k = 0; k < User.Color.Length; k++)
-                {
-                    User.Color[k] = Console.ReadLine();
-
-                }
-
-
-            }
-            
+            ShowColor();
 
             Console.ReadKey();
         }
