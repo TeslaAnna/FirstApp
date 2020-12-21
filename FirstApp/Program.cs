9e64ae7288feb396e663ca4366b12ba2687b9728
TeslaAnna <tesla.anna@gmail.com>
2020-12-21 16:11:21 +0200

    5.2.8 разделение метода GetArrayFromConsole() на GetArrayFromConsole() и SortArray()

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,71 +4,43 @@ namespace FirstApp
 {
     class Program
     {
-        static string ShowColors(string username, int userage)
+        static int[] GetArrayFromConsole()
         {
-            Console.WriteLine("{0}, {1} лет \n Напишите свой любимый цвет на английском с маленькой буквы", username, userage);
-            var color = Console.ReadLine();
-            switch (color)
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
             {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Your color is red!");
-                    break;
-
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Your color is green!");
-                    break;
-
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Your color is cyan!");
-                    break;
-
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Your color is yellow!");
-                    break;
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
             }
+                        
+            return result;
 
+        }
+        static int[] SortArray(int[] result)
+        {
+            
+            int temp = 0;
 
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = i + 1; j < result.Length; j++)
+                    if (result[i] > result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
 
-            return color;
+                    }
+            }
+           
+
+            return result;
         }
         static void Main(string[] args)
 
         {
-            var (name, age) = ("Анна", 34);
-
-            Console.WriteLine("Введите ваше имя: ");
-            name = Console.ReadLine();
-
-            Console.WriteLine("Введите ваш возраст цифрами: ");
-            age = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Ваше имя: {0}", name);
-            Console.WriteLine("Ваш возраст: {0} ", age);
-
-            var FavColors = new string[3];
-
-            for (int i = 0; i < FavColors.Length; i++)
-            {
-                FavColors[i] = ShowColors(name, age);
-            }
-
-            Console.WriteLine("Ваши любимые цвета:");
-            foreach (var color in FavColors)
-            {
-                Console.WriteLine(color);
-            }
-
-
-
-
+            
 
             Console.ReadKey();
         }
