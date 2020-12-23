dfdd743327a9fdd8af5b974b8e8270bf36d23219
TeslaAnna <tesla.anna@gmail.com>
2020-12-23 14:49:05 +0200

    5.3.1 Создание метода ChangeAge и проверка результатов его работы.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,61 +4,35 @@ namespace FirstApp
 {
     class Program
     {
-        static int[] GetArrayFromConsole(int num = 5)
-        {
-            var result = new int[num];
-
-            for (int i = 0; i < result.Length; i++)
-            {
-                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
-                result[i] = int.Parse(Console.ReadLine());
-            }
-                        
-            return result;
-
-        }
-
-        static void ShowArray(int[] array, bool IsSort = false)
-        {
-            var temp = array;
-            if (IsSort)
-            {
-                temp = SortArray(array);
-            }
-
-            foreach(var item in temp)
-            {
-                Console.WriteLine(item);
-            }
-              
-        }
-        static int[] SortArray(int[] result)
-        {
-            
-            int temp = 0;
-
-            for (int i = 0; i < result.Length; i++)
-            {
-                for (int j = i + 1; j < result.Length; j++)
-                    if (result[i] > result[j])
-                    {
-                        temp = result[i];
-                        result[i] = result[j];
-                        result[j] = temp;
-
-                    }
-            }
-           
-
-            return result;
-        }
+        
         static void Main(string[] args)
 
         {
-            var array = GetArrayFromConsole(10);
-            ShowArray(array, true);
+            var someName = "Евгения";
+            Console.WriteLine(someName);
+
+            GetName(someName);
+            Console.WriteLine(someName);
+
+            var someAge = 34;
+            Console.WriteLine(someAge);
+
+            ChangeAge(someAge);
+            Console.WriteLine(someAge);
 
             Console.ReadKey();
+        }
+        
+        static void GetName(string name)
+        {
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine();
+        }
+
+        static void ChangeAge(int age)
+        {
+            Console.WriteLine("Введите возраст");
+            age = Convert.ToInt32(Console.ReadLine());
         }
 
     }
