96d336a523149680acb7598b3396ff4432289529
TeslaAnna <tesla.anna@gmail.com>
2020-12-28 13:03:02 +0200

    5.5.3 Программа имитирующая эхо (-2 буквы при каждом выводе)

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,7 +4,7 @@ namespace FirstApp
 {
     class Program
     {
-        
+
         static void Main(string[] args)
 
         {
@@ -16,23 +16,31 @@ namespace FirstApp
 
             var deep = int.Parse(Console.ReadLine());
 
-           Echo(str, deep);
-            
+            Echo(str, deep);
+
 
             Console.ReadKey();
         }
 
-        static void Echo(string phrase, int deep)
+        static void Echo(string saidworld, int deep)
         {
-            Console.WriteLine(phrase);
+            var modif = saidworld;
+
+
+            if (modif.Length > 2)
+            {
+                modif = modif.Remove(0, 2);
+            }
+
+            Console.WriteLine("..." + modif);
 
             if (deep > 1)
             {
-                Echo(phrase, deep - 1);
+                Echo(modif, deep - 1);
             }
         }
 
-        
+
 
     }
 
