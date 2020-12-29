b662286bd3e326ac7bba1b958613df01ab5384c8
TeslaAnna <tesla.anna@gmail.com>
2020-12-29 20:28:03 +0200

    Задание 5.3.12

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,6 +6,7 @@ namespace FirstApp
     {
         static int[] GetArrayFromConsole(int num = 5)
         {
+
             var result = new int[num];
 
             for (int i = 0; i < result.Length; i++)
@@ -13,7 +14,7 @@ namespace FirstApp
                 Console.WriteLine("Введите элемент массива номер {0}", i + 1);
                 result[i] = int.Parse(Console.ReadLine());
             }
-                        
+
             return result;
 
         }
@@ -21,45 +22,82 @@ namespace FirstApp
         static void ShowArray(int[] array, bool IsSort = false)
         {
             var temp = array;
+
+            int[] sasc;
+            int[] sdesc;
+
             if (IsSort)
             {
-                temp = SortArray(array);
+                temp = SortArray(array, out sdesc, out sasc);
             }
 
-            foreach(var item in temp)
+            foreach (var item in temp)
             {
                 Console.WriteLine(item);
             }
-              
+
         }
-        static int[] SortArray(int[] result)
+        static void SortArray(in int[] array, out int[] sortdesc, out int[] sortasc)
         {
-            
+
+            sortdesc = SortArrayDesc(array);
+            sortasc = SortArrayAsc(array);
+
+        }
+
+        static int[] SortArrayDesc(int[] desc)
+        {
             int temp = 0;
 
-            for (int i = 0; i < result.Length; i++)
+            for (int i = 0; i < desc.Length; i++)
             {
-                for (int j = i + 1; j < result.Length; j++)
-                    if (result[i] > result[j])
+                for (int j = i + 1; j < desc.Length; j++)
+                    if (desc[i] < desc[j])
                     {
-                        temp = result[i];
-                        result[i] = result[j];
-                        result[j] = temp;
+                        temp = desc[i];
+                        desc[i] = desc[j];
+                        desc[j] = temp;
 
                     }
             }
-           
 
-            return result;
+
+            return desc;
+        }
+
+        static int[] SortArrayAsc(int[] asc)
+        {
+            int temp = 0;
+
+            for (int i = 0; i < asc.Length; i++)
+            {
+                for (int j = i + 1; j < asc.Length; j++)
+                    if (asc[i] > asc[j])
+                    {
+                        temp = asc[i];
+                        asc[i] = asc[j];
+                        asc[j] = temp;
+
+                    }
+            }
+
+
+            return asc;
         }
         static void Main(string[] args)
 
         {
             var array = GetArrayFromConsole(10);
             ShowArray(array, true);
-
             Console.ReadKey();
+
         }
+
+
+
+
+
+
 
     }
 
