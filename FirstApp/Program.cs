c7823540ba7fca623dc3492691cc0ae0a63a4281
TeslaAnna <tesla.anna@gmail.com>
2021-01-31 23:17:18 +0200

    5.6. Итоговый проект (Анкета)

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,36 +2,121 @@
 
 namespace FirstApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+
+        private static (string Name, string SurName, int Age, string[] PetNames, string[] FavoriteColors) GetUserData()
+        {
+            Console.WriteLine("Введите ваше имя:");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine("Введите вашу фамилию:");
+            var userSurName = Console.ReadLine();
+
+            var hasCorrectAge = false;
+            var age = 0;
+            while (!hasCorrectAge)
+            {
+                Console.WriteLine("Введите ваш возраст:");
+                var stringAge = Console.ReadLine();
+                hasCorrectAge = ParseIntValue(stringAge, out age);
+            }
+            
+            Console.WriteLine("У вас есть домашние животные: Да или Нет?");
+            string stringHavePets = Console.ReadLine();
+            var havePets = false;
+            if (stringHavePets == "Да")
+            {
+                havePets = true;
+            }
+
+            var petNames = new string[0];
+            if (havePets)
+            {
+                var petsCount = 0;
+                var hasCorrectPetsCount = false;
+                while (!hasCorrectPetsCount)
+                {
+                    Console.WriteLine("Введите количество питомцев:");
+                    var stringPetsCount = Console.ReadLine();
+                    hasCorrectPetsCount = ParseIntValue(stringPetsCount, out petsCount);
+                }
+                petNames = GetPetNames(petsCount);
+            }
+
+            var favoriteColors = new string[0];
+            var colorsCount = 0;
+            var hasCorrectColorsCount = false;
+            while (!hasCorrectColorsCount)
+            {
+                Console.WriteLine("Введите количество любимых цветов:");
+                var stringColorsCount = Console.ReadLine();
+                hasCorrectColorsCount = ParseIntValue(stringColorsCount, out colorsCount);
+            }
+            favoriteColors = GetFavoriteColors(colorsCount);
+
+            return (userName, userSurName, age, petNames, favoriteColors);
+        }
+
+        private static bool ParseIntValue(string inputValue, out int intValue)
+        {
+            var result = Int32.TryParse(inputValue, out intValue);
+            if (!result || intValue <= 0)
+            {
+                result = false;
+                Console.WriteLine("Введите положительное целое число.");
+            }
+            
+            return result;
+        }
+
+        private static string[] GetFavoriteColors(int colorsCount)
+        {
+            var favoriteColors = new string[colorsCount];
+            for (var i = 0; i < colorsCount; i++)
+            {
+                Console.WriteLine($"Введите любимый цвет: {i + 1}:");
+                favoriteColors[i] = Console.ReadLine();
+            }
+            return favoriteColors;
+        }
+
+        private static string[] GetPetNames(int petsCount)
+        {
+            var petNames = new string[petsCount];
+            for (var i = 0; i < petsCount; i++)
+            {
+                Console.WriteLine($"Введите имя питомца {i + 1}:");
+                petNames[i] = Console.ReadLine();
+            }
+            return petNames;
+        }
+
+        private static void PrintUserData((string Name, string SurName, int Age, string[] PetNames, string[] FavoriteColors) userData)
+        {
+            Console.WriteLine("Имя пользователя:");
+            Console.WriteLine(userData.Name);
+            Console.WriteLine("Фамилия пользователя:");
+            Console.WriteLine(userData.SurName);
+            Console.WriteLine("Возраст пользователя:");
+            Console.WriteLine(userData.Age);
+            if (userData.PetNames.Length > 0)
+            {
+                Console.WriteLine("Имена питомцев:");
+                Console.WriteLine(String.Join(", ", userData.PetNames));
+            }
+            Console.WriteLine("Любимые цвета:");
+            Console.WriteLine(String.Join(", ", userData.FavoriteColors));
+
+        }
+
+        private static void Main(string[] args)
 
         {
-            (string Name, string Type, double Age, int NameCount) Pet;
-
+            var userData = GetUserData();
+            Console.WriteLine();
+            PrintUserData(userData);
             
-            Console.WriteLine("Введите имя вашего питомца: ");
-            Pet.Name = Console.ReadLine();
-            Pet.NameCount = Pet.Name.Length;
-
-            Console.WriteLine("Введите тип вашего питомца: ");
-            Pet.Type = Console.ReadLine();
-
-            Console.WriteLine("Введите возраст вашего питомца цифрами: ");
-            Pet.Age = Convert.ToDouble(Console.ReadLine());
-            
-
-
-
-
-            Console.WriteLine("Имя вашего питомца: {0}", Pet.Name);
-            Console.WriteLine("Тип вашего животного: {0}", Pet.Type);
-            Console.WriteLine("Возраст вашего питомца: {0} ", Pet.Age);
-            Console.WriteLine("Длина имени вашего питомца: {0}", Pet.NameCount);
-            
-
-            
-
             Console.ReadKey();
         }
 
